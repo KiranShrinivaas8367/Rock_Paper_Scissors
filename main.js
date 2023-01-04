b49902adaa763ffe13389d8d6153f50f61f2720a
kiran shrinivaas <hayabuzakiran@gmail.com>
2023-01-04 18:02:01 +0530

    makeselection->added

--- main.js
+++ main.js
@@ -1,22 +1,6 @@
 const selectButtons = document.querySelectorAll('[data-selection]')
+const finalColumn = document.querySelector('[data-final-column]')
 
-const selections = [
-    {
-        name: 'rock',
-        emoji: '👊',
-        defeats: 'scissors'
-    },
-    {
-        name: 'paper',
-        emoji: '✋',
-        defeats: 'rock'
-    },
-    {
-        name: 'scissors',
-        emoji: '✌️',
-        defeats: 'paper'
-    }
-]
 selectButtons.forEach(selectButton => 
 {
     selectButton.addEventListener('click',() => {
@@ -28,8 +12,25 @@ selectButtons.forEach(selectButton =>
 
 function makeSelection(selection){
     // console.log(selection);
-    const computerSelection=randomGeneration();
+    const computerSelection = randomGeneration();
+    const yourWinner = isWinner(selection,computerSelection)
+    const computerWinner = isWinner(computerSelection,selection)
+
+    addSelectionResult(computerSelection,computerWinner)
+    addSelectionResult(selection,yourWinner)
     console.log(computerSelection);
+}
+
+function addSelectionResult(selection,winner){
+    const div = document.createElement('div')
+    div.innerText=selection.emoji
+    div.classList.add('result-selection')
+    if(winner) div.classList.add('winner')
+    finalColumn.after(div)
+}
+
+function isWinner(selection,opponentSelection){
+    return selection.defeats == opponentSelection.name;
 }
 
 function randomGeneration(){
